4c37a4e686c0ef8119719afd61942feb131d57de
jagatkhatri01 <jagatkhatri383@gmail.com>
2023-12-01 09:05:01 +0545

    added views and templates

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Comment
 
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, "home/index.html", context = context)
